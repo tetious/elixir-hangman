b45d11cb03ef6802c729f22bc49dd294572ff5c7
Greg Lincoln <greg@greglincoln.com>
2023-01-28 18:48:44 -0500

    Make the agent based implementation more-better.

--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -1,7 +1,8 @@
 defmodule Dictionary.Runtime.Server do
   alias Dictionary.Impl.WordList
+  @me __MODULE__
   @type t :: pid
 
-  def start_link(), do: Agent.start_link(&WordList.word_list/0)
-  def random_word(pid), do: Agent.get(pid, &WordList.random_word/1)
+  def start_link(), do: Agent.start_link(&WordList.word_list/0, name: @me)
+  def random_word(), do: Agent.get(@me, &WordList.random_word/1)
 end
--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -11,8 +11,7 @@ defmodule Hangman.Impl.Game do
 
   @spec new_game :: t()
   def new_game do
-    Dictionary.start()
-    |> Dictionary.random_word()
+    Dictionary.random_word()
     |> new_game()
   end
 
