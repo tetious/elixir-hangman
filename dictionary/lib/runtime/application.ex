83584da08c5e867efd516288b8d5c6b89b7c3399
Greg Lincoln <greg@greglincoln.com>
2023-01-28 20:28:40 -0500

    Convert hangman to a GenServer.

--- dictionary/lib/runtime/application.ex
+++ dictionary/lib/runtime/application.ex
@@ -1,5 +1,15 @@
 defmodule Dictionary.Runtime.Application do
   use Application
 
-  def start(_type, _args), do: Dictionary.Runtime.Server.start_link()
+  def start(_type, _args) do
+    children = [
+      { Dictionary.Runtime.Server, [] }
+    ]
+    options = [
+      name: Dictionary.Runtime.Supervisor,
+      strategy: :one_for_one
+    ]
+
+    Supervisor.start_link(children, options)
+  end
 end
--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -1,8 +1,10 @@
 defmodule Dictionary.Runtime.Server do
+  use Agent
   alias Dictionary.Impl.WordList
   @me __MODULE__
   @type t :: pid
 
-  def start_link(), do: Agent.start_link(&WordList.word_list/0, name: @me)
+  def start_link(_), do: Agent.start_link(&WordList.word_list/0, name: @me)
+
   def random_word(), do: Agent.get(@me, &WordList.random_word/1)
 end
