f90a61d2bc467d20e3ab3705c404effc4332da68
Greg Lincoln <greg@greglincoln.com>
2023-01-29 14:49:50 -0500

    Not ideal but kinda working.

--- b1/lib/b1_web/controllers/hangman_controller.ex
+++ b1/lib/b1_web/controllers/hangman_controller.ex
@@ -4,4 +4,20 @@ defmodule B1Web.HangmanController do
   def index(conn, _params) do
     render(conn, :index)
   end
+
+  def new(conn, _params) do
+    game = Hangman.new_game()
+    tally = Hangman.tally(game)
+
+    conn
+    |> put_session(:game, game)
+    |> render(:game, tally: tally)
+  end
+
+  def update(conn, params) do
+    guess = params["make_move"]["guess"]
+    game = get_session(conn, :game)
+    tally = Hangman.make_move(game, guess)
+    render(conn, :game, tally: tally)
+  end
 end
